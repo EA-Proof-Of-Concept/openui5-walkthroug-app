f18c2abcbf04c193e8708677ddf90b52f6b1b49d
Eric A. Rodríguez S. <74212894+Eazicom@users.noreply.github.com>
2025-01-22 18:31:54 -0600

    ADD: Paso 3.

--- step_03/webapp/index.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-	"sap/m/Text"
-], (Text) => {
-	"use strict";
-
-	new Text({
-		text: "¡Hola desde OpenUI5!"
-	}).placeAt("content");
-});
---
+++ steps/03/webapp/index.ts
@@ -0,0 +1,6 @@
+import Text from 'sap/m/Text';
+
+const text = new Text( {
+    text: '¡Hola soy OpenUI5!',
+} );
+text.placeAt( 'content' );
